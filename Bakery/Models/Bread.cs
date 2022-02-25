df474c5ecb0791a8ecbe60e1588746c09ad86c70
Brenna Lavin <lavinbrenna@gmail.com>
2022-02-25 10:28:17 -0800

    add bread int loaf constructor, pass loaf count test

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -4,6 +4,11 @@ namespace Bakery.Models
 {
   public class Bread
   {
-    
+    public int LoafCount {get; set;}
+
+    public Bread(int loafCount)
+    {
+      LoafCount = loafCount;
+    }
   }
 }
